f7d319bf6b78c19e857abd83d4550d427357b8a5
Axel75018 <axel.calvet@gmail.com>
2023-06-05 14:53:41 +0200

    404 wip css a finir

--- src/pages/404/PageAbsente.jsx
+++ src/pages/404/PageAbsente.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const PageAbsente = () => {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="page404">
+      <h1 className="er404">404</h1>
+      <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      <NavLink to="/" className="linkAccueil">
+        Retourner sur la page d’accueil
+      </NavLink>
     </div>
   );
 };
--- src/pages/logements/Logements.jsx
+++ src/pages/logements/Logements.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import logementjson from "../../data/logements.json";
+import logementJson from "../../data/logements.json";
 
 const Logements = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const isValidId = logementjson.find((logement) => logement.id === id);
+  const isValidId = logementJson.find((logement) => logement.id === id);
 
   useEffect(() => {
     //exécuté au "mount" du composant
@@ -18,9 +18,15 @@ const Logements = () => {
     return null; // Cette ligne reste importante pour éviter un rendu avant la redirection
   } else {
     // rendre le composant normalement
+    const ficheLogement = logementJson.find((logement) => logement.id === id);
+
     return (
       <div>
         <h1>Logements ok</h1>
+        <article>
+          <img src={ficheLogement.cover} alt={ficheLogement.title} />
+          <h2>{ficheLogement.title}</h2>
+        </article>
       </div>
     );
   }
