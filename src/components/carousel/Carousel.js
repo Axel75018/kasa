a2b38622359cde41802feafded3f37f70c0383d8
Axel75018 <axel.calvet@gmail.com>
2023-06-07 13:06:31 +0200

    carousel wip position ok

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-
+import chevronGauche from "../../assets/images/chevronGauche.png";
+import chevronDroite from "../../assets/images/chevronDroite.png";
 const Carousel = ({ pictures }) => {
   // on utilise pictures qui vient des props
   const [index, setIndex] = useState(0);
 
   return (
     <section className="carousel">
+      <img className="chevron gauche" src={chevronGauche} alt="Précédent" />
+      <img className="chevron droite" src={chevronDroite} alt="Suivant" />
       {pictures.map((image, index) => (
         <div className="carousel__container">
           <img
