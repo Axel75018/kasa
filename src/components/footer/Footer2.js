117b060740623bcabdf135031be843d49df6bddf
Axel Calvet <axel.calvet@gmail.com>
2023-06-15 14:45:26 +0200

    ok mobile tab wip

--- src/components/footer/Footer2.js
+++ src/components/footer/Footer2.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import LOGOnb from "../../assets/images/LOGOnb.png";
 
 const Footer2 = () => {
   return (
     <div>
       <footer>
-        <img src={LOGOnb} alt="Kasa" className="LogoFooter"></img>
+      <NavLink to="/">
+        <img src={LOGOnb} alt="Kasa" className="LogoFooter"/></NavLink>
         <p>© 2020 Kasa. All rights reserved</p>
       </footer>
     </div>
