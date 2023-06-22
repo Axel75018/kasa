352ed323aa1ec86cfe8d389e82714d1e446a66d9
Axel Calvet <axel.calvet@gmail.com>
2023-06-22 10:52:38 +0200

    correction style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import styles from "../src/assets/style/style.css";
+import './assets/style/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
